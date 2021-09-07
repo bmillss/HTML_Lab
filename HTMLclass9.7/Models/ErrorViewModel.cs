48c4cd4f90d29d8c4475f6bb70769b9ba4d962d5
Brandon Miller <Brandonmiller1@quickenloans.com>
2021-09-07 15:38:44 -0400

    Initial commit

---
+++ HTMLclass9.7/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTMLclass9._7.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
